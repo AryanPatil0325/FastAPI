94fe6595d33a538191b2205a3c5cb32b42f751d9
Aryan Patil <patilavs03@gmail.com>
2026-09-08 21:13:56 +0530

    Authentication and Swagger authentication

--- Practice/books.py
+++ Practice/books.py
@@ -1,16 +1,32 @@
 # file that holds the endpoints
-from typing import Optional,Annotated
+from datetime import datetime, timedelta, timezone
+from sqlite3 import IntegrityError
+from typing import Optional,Annotated,Literal
 
 from fastapi import APIRouter,HTTPException,Path,Query,Depends
 from starlette import status
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, ConfigDict, EmailStr,Field
 
 from database import SessionFactory
 from sqlalchemy.orm import Session
 
-from model import Books
+from model import Books,Users
+
+from passlib.context import CryptContext
+
+from jose import jwt,JWTError
+
+from fastapi.security import OAuth2PasswordBearer
+# from Swagger authenticator
+from fastapi.security import HTTPBearer,HTTPAuthorizationCredentials
 
 router = APIRouter()
+
+# Swagger
+security = HTTPBearer()
+
+SECRET_KEY = "aryan"
+ALGORITHM = "HS256"
 
 
 def get_db():
@@ -22,6 +38,11 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto') # used for password hashing using bcrypt
+
+# get the JWT bearer token
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="login") # endpoint which return token
+
 # Pydantic model for validations
 class Book_Request(BaseModel):
     # id:Optional[int] = Field(description="ID field is not required",default=None)
@@ -31,16 +52,79 @@ class Book_Request(BaseModel):
     price:int = Field(gt=0)
     owner_id:int = Field(gt=0)
 
+class User_Request(BaseModel):
+    username:str = Field(min_length=3)
+    email:EmailStr # to validate the email field
+    password:str = Field(min_length=5,max_length=16)
+    role:Literal["user","admin"]
+
+# Book Response class which will return the SQLAlchemy object into json
+# SQLAlchemy -> pydandic model -> json 
+class Book_Response(BaseModel):
+    id:int
+    title:str 
+    author:str
+    category:str 
+    price:int 
+    owner_id:int
+
+    # convert the SQLalchemy object into pydantic
+    model_config = ConfigDict(from_attributes=True)
+
+# User Response class -> SQLalchemy to json
+class User_Response(BaseModel):
+    id:int
+    username:str
+    email:str
+    role:str
+
+    model_config = ConfigDict(from_attributes=True)
+
+# function to create JWT token
+def create_access_token(username:str,user_id:int,expiry_time:timedelta):
+    encode = {"sub":username,"id":user_id}
+    expiry = datetime.now(timezone.utc) + expiry_time
+    encode.update({"exp":expiry})
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+
+# function to verify the token created
+def verify_token(token:str):
+    # handling tampered or expired tokens
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username:str = payload.get('sub')
+        user_id: int = payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=401,detail="Couldn't verify the user")
+        return {"username":username,"id":user_id}
+    except JWTError:
+        raise HTTPException(status_code=401,detail="Couldn't verify the user")
+
+# Function that gets Authentication Bearer <JWT> and extracts it 
+# def get_current_user(token:Annotated[str,Depends(oauth2_bearer)]):
+#     return verify_token(token)
+
+# Swagger credentials
+async def get_current_user(
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]
+):
+    token = credentials.credentials
+    return verify_token(token)
+
+# authentication dependency to check for valid jwt
+auth_dependency = Annotated[dict,Depends(get_current_user)]
+
 
 # List of books
 # Books = [
 #     {
 #         "id": 1,
-#         "title": "Atomic Habits",
-#         "author": "James Clear",
-#         "category": "Self-Help",
-#         "price": 499,
-#         "owner_id": 1
+        # "title": "Atomic Habits",
+        # "author": "James Clear",
+        # "category": "Self-Help",
+        # "price": 499,
+        # "owner_id": 1
 #     },
 #     {
 #         "id": 2,
@@ -52,11 +136,11 @@ class Book_Request(BaseModel):
 #     },
 #     {
 #         "id": 3,
-#         "title": "Clean Code",
-#         "author": "Robert C. Martin",
-#         "category": "Programming",
-#         "price": 799,
-#         "owner_id": 1
+        # "title": "Clean Code",
+        # "author": "Robert C. Martin",
+        # "category": "Programming",
+        # "price": 799,
+        # "owner_id": 1
 #     },
 #     {
 #         "id": 4,
@@ -74,14 +158,14 @@ class Book_Request(BaseModel):
 #         "price": 599,
 #         "owner_id": 2
 #     },
-#     {
-#         "id": 6,
-#         "title": "The Psychology of Money",
-#         "author": "Morgan Housel",
-#         "category": "Finance",
-#         "price": 399,
-#         "owner_id": 3
-#     },
+    # {
+    #     "id": 6,
+    #     "title": "The Psychology of Money",
+    #     "author": "Morgan Housel",
+    #     "category": "Finance",
+    #     "price": 399,
+    #     "owner_id": 3
+    # },
 #     {
 #         "id": 7,
 #         "title": "Python Crash Course",
@@ -100,7 +184,7 @@ class Book_Request(BaseModel):
 #     }
 # ]
 
-@router.get("/books",status_code=status.HTTP_200_OK)
+@router.get("/books",status_code=status.HTTP_200_OK,response_model = list[Book_Response]) # Response model is for single book but we are getting multiple books in list
 async def get_all_books(
     db:db_dependency,
     author:Optional[str]=Query(None,min_length=3),
@@ -111,7 +195,7 @@ async def get_all_books(
     
     if author is not None:
         # filtered_books = [book for book in filtered_books if book.get('author').casefold() == author.casefold()]
-        filtered_books = db.query(Books).filter(Books.author.ilike(author.casefold()))
+        filtered_books = filtered_books.filter(Books.author.ilike(author.casefold()))
     
     if category is not None:
         # filtered_books = [book for book in filtered_books if book.get('category').casefold() == category.casefold()]
@@ -129,12 +213,13 @@ async def get_all_books(
     return book_query
     
 
-@router.post("/new_book",status_code=status.HTTP_201_CREATED)
+@router.post("/new_book",status_code=status.HTTP_201_CREATED,response_model=Book_Response)
 async def create_new_book(db:db_dependency,new_book:Book_Request):
     book_model = Books(**new_book.model_dump())
     db.add(book_model)
     db.commit()
-    return{"message":"Book created successfully"}
+    db.refresh(book_model)
+    return book_model
 
 # @router.put("/update_book/",status_code=status.HTTP_200_OK)
 # async def update_book(db:db_dependency,updated_book:Book_Request,id:int=Query(gt=0)):
@@ -147,7 +232,7 @@ async def create_new_book(db:db_dependency,new_book:Book_Request):
 #             return{"message":"Book updated successfully"}
 #     raise HTTPException(status_code=404,detail="ID not found")
 
-@router.put("/update_book/",status_code=status.HTTP_200_OK)
+@router.put("/update_book/",status_code=status.HTTP_200_OK,response_model=Book_Response)
 async def update_book(db:db_dependency,updated_book:Book_Request,id:int=Query(gt=0)):
     book_model = db.query(Books).filter(Books.id == id).first()
     if book_model is None:
@@ -160,7 +245,7 @@ async def update_book(db:db_dependency,updated_book:Book_Request,id:int=Query(gt
     book_model.owner_id = updated_book.owner_id
 
     db.commit()
-
+    return book_model
 
 # @router.delete("/delete_book/{book_id}",status_code=status.HTTP_200_OK)
 # async def delete_book(book_id:int=Path(gt=0)):
@@ -186,10 +271,70 @@ async def delete_book(db:db_dependency,book_id:int=Path(gt=0)):
 #             return book
 #     raise HTTPException(status_code=404,detail="ID not found")
 
-@router.get("/books/{book_id}",status_code=status.HTTP_200_OK)
-async def get_book_by_id(db:db_dependency,book_id:int=Path(gt=0)):
+# current user will first check the authenticity of jwt token and then execute the endpoint if valid
+@router.get("/books/{book_id}",status_code=status.HTTP_200_OK,response_model=Book_Response)
+async def get_book_by_id(db:db_dependency,current_user:auth_dependency,book_id:int=Path(gt=0)):
     book_model = db.query(Books).filter(Books.id == book_id).first()
     if book_model is None:
         raise HTTPException(status_code=404,detail="ID not found")
     return book_model
 
+
+@router.post("/create_user",status_code=status.HTTP_201_CREATED,response_model=User_Response)
+async def create_new_user(db:db_dependency,new_user:User_Request):
+    # users_model = Users(**new_user.model_dump())
+    # hashing password
+    users_model = Users(
+        username = new_user.username,
+        email = new_user.email,
+        password = bcrypt_context.hash(new_user.password),
+        role = new_user.role
+    )
+    # check for duplicate username and email field at database level since we are using "unique=True"
+    try:
+        db.add(users_model)
+        db.commit()
+        db.refresh(users_model)
+        return users_model
+        
+    except IntegrityError:
+        db.rollback() # reset the uncommitted state to normal to reuse it again
+        raise HTTPException(status_code=409,detail="Username or Email already exists")
+
+
+
+# 1. Receive username/email + password
+#              ↓
+# 2. Find the user in database
+#              ↓
+# 3. Get stored password hash
+#              ↓
+# 4. Verify supplied password against hash
+#              ↓
+# 5. Correct?
+#        ↓              ↓
+#       YES             NO
+#        ↓              ↓
+#   Generate JWT     Reject login
+class Login_Request(BaseModel):
+    username:str
+    password:str
+
+
+@router.post("/login")
+async def create_jwt_token(db:db_dependency,creds:Login_Request):
+    # find user in database
+    user_model = db.query(Users).filter(Users.username == creds.username).first()
+    if user_model is None:
+        raise HTTPException(status_code=401,detail=f"User of {creds.username} not found")
+    # get stored hashed
+    stored_hashed = user_model.password
+    if bcrypt_context.verify(creds.password,stored_hashed):
+        # generate JWT
+        token = create_access_token(creds.username,user_model.id,timedelta(minutes=20))
+        return token
+        
+    else:
+        raise HTTPException(status_code=401,detail='Invalid username or password')
+
+   
--- Practice/model.py
+++ Practice/model.py
@@ -1,3 +1,4 @@
+# Books table
 # | Column   | Type    | Requirement |
 # | -------- | ------- | ----------- |
 # | id       | INTEGER | PRIMARY KEY |
@@ -7,8 +8,31 @@
 # | price    | INTEGER | NOT NULL    |
 # | owner_id | INTEGER | NOT NULL    |
 
+# Users Table
+# | Column     | Type    | Requirements         |
+# | ---------- | ------- | -------------------- |
+# | `id`       | Integer | Primary key, indexed |
+# | `username` | String  | Not nullable         |
+# | `email`    | String  | Not nullable         |
+# | `password` | String  | Not nullable         |
+# | `role`     | String  | Not nullable         |
+
+
 from database import Base
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,ForeignKey
+from sqlalchemy.orm import relationship
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,index=True)
+    username = Column(String,nullable=False,unique=True)
+    email = Column(String,nullable=False,unique=True)
+    password = Column(String,nullable=False,unique=True)
+    role = Column(String,nullable=False)
+
+    # adding the relationship between users and books -> 1:m -> represent Python object connection
+    books = relationship("Books",back_populates="owner")
+
 
 class Books(Base):
     __tablename__ = "books"
@@ -17,4 +41,7 @@ class Books(Base):
     author = Column(String,nullable=False)
     category = Column(String,nullable=False)
     price = Column(Integer,nullable=False)
-    owner_id = Column(Integer,nullable=False)
+    owner_id = Column(Integer,ForeignKey("users.id"),nullable=False)
+
+    owner = relationship("Users",back_populates="books")
+
