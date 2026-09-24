318ba29436e2f2022f574569003ad12314304172
Aryan Patil <patilavs03@gmail.com>
2026-09-24 20:37:43 +0530

    Connected to MySQL

--- Projects/Project3/Todo_App/database.py
+++ Projects/Project3/Todo_App/database.py
@@ -6,10 +6,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # path to database link
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosApp.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosApp.db" # sqlite3 database 
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:aryan03@127.0.0.1:3306/todoapplicationdatabase" # mysql database 
 
 # creating engine of database to connect 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False}) # use multiple-threads to check connection
+# engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False}) # use multiple-threads to check connection (for sqlite3)
+engine = create_engine(SQLALCHEMY_DATABASE_URL) # for mysql
 
 # create session factory for sessions 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
